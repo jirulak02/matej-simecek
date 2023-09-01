3f8950994e6e98d0a73aedc498c8bfb6e506f297
jirulak02 <jirulak02@gmail.com>
2023-09-01 17:01:42 +0200

    adjust release date

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="flex flex-col items-center">
       <div className="flex h-screen w-screen flex-col items-center justify-center bg-black">
         <h1 className="text-4xl text-white">Tato stránka se aktuálně připravuje!</h1>
-        <h2 className="text-2xl text-white">Vraťte se v září, díky.</h2>
+        <h2 className="text-2xl text-white">Vraťte se v druhé půlce září, díky.</h2>
       </div>
 
       {/* <HeroSection />
