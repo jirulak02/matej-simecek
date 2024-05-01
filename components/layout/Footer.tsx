5c74064f44ef814651010eefbecafacd8190fc00
jirulak02 <jirulak02@gmail.com>
2024-05-01 20:22:31 +0200

    chore: change footer text

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -21,13 +21,17 @@ export default function Footer() {
           />
         </div>
         <p className="text-center text-xs">
-          Investiční produkty zprostředkovávám jako vázaný zástupce zapsaný v registru ČNB pod IČO:
-          10674209 a zároveň registrovaný pod investičním zprostředkovatelem Broker Trust, a.s. IČO:
-          26439719. Pojistné produkty zprostředkovávám jako podřízený pojišťovací zprostředkovatel
-          zapsaný v registru ČNB pod č. PPZ a zároveň registrovaný pod pojišťovacím agentem Broker
-          Trust, a.s. 26439719. Spotřebitelské úvěry zprostředkovávám jako vázaný zástupce zapsaný v
-          registru ČNB pod IČO: 1067420 a zároveň registrovaný pod samostatným zprostředkovatelem
-          spotřebitelských úvěrů Broker Trust, a.s. IČO: 26439719
+          Vázaný zástupce (IČO: 10674209) jednající jménem společnosti Broker Trust, a.s., která je
+          v registru ČNB vedena jako Samostatný a Investiční zprostředkovatel pod IČO: 26439719.
+          Zprostředkovává tedy pojištění, úvěry a investice. Bližší informace získáte na{" "}
+          <Link
+            href="https://www.brokertrust.cz/proklienty"
+            target="_blank"
+            className="whitespace-nowrap text-xs hover:underline"
+          >
+            https://www.brokertrust.cz/proklienty
+          </Link>
+          .
         </p>
         <div className="my-6 flex flex-col items-center gap-6 text-base sm:flex-row sm:gap-8 md:my-8">
           <Link href="https://www.fair-life.cz/" target="_blank" className="whitespace-nowrap">
@@ -42,7 +46,7 @@ export default function Footer() {
           </Link>
           <hr className="mt-auto hidden w-full border-t-2 pb-3 sm:block" />
           <Link
-            href="https://www.linkedin.com/in/mat%C4%9Bj-%C5%A1ime%C4%8Dek-71aba6215"
+            href="https://www.linkedin.com/in/mat%C4%9Bj-%C5%A1ime%C4%8Dek"
             target="_blank"
             rel="noopener noreferrer"
           >
